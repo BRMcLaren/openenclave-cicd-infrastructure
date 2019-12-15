3132ff76d7ef36178bbe51028e676b427a45ec87
brmclare <brmclare@microsoft.com>
2019-12-15 10:51:41 +0000

    add non sgx jenkins slave

--- network.tf
+++ network.tf
@@ -49,3 +49,20 @@ resource "azurerm_network_interface" "Jenkins1804ACC" {
         environment = "Terraform Demo"
     }
 }
+
+resource "azurerm_network_interface" "Jenkins1804NonSGX" {
+    name                      = "Jenkins1804NonSGX"
+    location                  = "eastus"
+    resource_group_name       = "${azurerm_resource_group.myterraformgroup.name}"
+    network_security_group_id = "${azurerm_network_security_group.myterraformnsg.id}"
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        private_ip_address_allocation = "Dynamic"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
--- vm.tf
+++ vm.tf
@@ -132,6 +132,51 @@ resource "azurerm_virtual_machine" "Jenkins1804ACC-1" {
 }
 
 
+resource "azurerm_virtual_machine" "Jenkins1804NonSGX-1" {
+    name                  = "Jenkins1804NonSGX-1"
+    location              = "eastus"
+    resource_group_name   = "${azurerm_resource_group.myterraformgroup.name}"
+    network_interface_ids = ["${azurerm_network_interface.Jenkins1804NonSGX.id}"]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+        name              = "Jenkins1804NonSGX-1"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "Premium_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = "1804UbuntuNonSGX"
+        admin_username = "azureuser"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path     = "/home/azureuser/.ssh/authorized_keys"
+            key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHgGBI68diMhO1v0Uz961KAiP4KR1W7EUTqtLOxR0qhsOzfnKZDuxbgsRXvZOYhqReBtig8zZrW6nUbBeo9wTKkw4B1/uFyFf4EShLlXMZSMYAtEa0JsLPjGBrcHY39BTVIlG/PtSG0yiTTgqcdnACZ40MvUrr65Vx7M0a3PVZLQJMh7vVhdem9XoUAFnOFGkHoO3OJo6PQ1m7z+x7+saGLEzT5h+Z6oScYCgjTAkzcuWU2SIuKWL5LvRa6NUIpuQ1eBUz94GrWZgCG29i6vt6cARUXUzPaW0TpJdvYgrmu4Ymb+NZQ1U8klYGvrJPhdBEqSHD2aJ5Bk9VbLV7om488J6P3hUW1OceUfO/6OZ+ls1486JaRJ0g2QVcc5zBo+QM6FK1ZovlCHYYiYLu2gTXuyol4JqZsRtyzvI1kZAXGvwAb1CThtFHT8FhMuHX/s7hQuUFcU+xc0G3YrodttfjcRsDwS5zpXaB0zovchedgCsORsyAnR9STBR0V2+/y14GXgKG82+KzCpdE1o8r3fsPXK2VHzeafu+YmwQiUJ/yQnUJXoL2Vg3TbuICnqX6axB3iceZketXBfuCYgmPPzB3SWVvTytl7qN6lEb6ZeBEM4wugQqRskko5xkhFDf2Qcbfr62QQEhrE6dmBzaEq4rJKivOJVVc2UthfjNUFIPOw== brmclare@microsoft.com"
+        }
+    }
+
+    boot_diagnostics {
+        enabled = "true"
+        storage_uri = "${azurerm_storage_account.mystorageaccount.primary_blob_endpoint}"
+    }
+
+    tags = {
+        environment = "Jenkins Terraform Demo"
+    }
+}
+
+
 #resource "azurerm_virtual_machine" "JenkinsWin16ACC-1" {
 #    name                  = "JenkinsWin16ACC-1"
 #    location              = "eastus"
